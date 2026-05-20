ee8c19c56d2493e3b179688d6d380386f3e4d79f
Cayzoxs <cahyamuhamad2005@gmail.com>
2026-05-20 12:33:56 +0700

    feat : implementasi migration dan seeding menggunakan library faker

---
+++ app/Database/Seeds/ProductSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        // Menggunakan locale Indonesia
+        $faker = \Faker\Factory::create('id_ID');
+
+        for ($i = 0; $i < 10; $i++) {
+            $data = [
+                'username'   => $faker->userName,
+                'email'      => $faker->email,
+                'password'   => password_hash('1234567', PASSWORD_DEFAULT),
+                'role'       => $faker->randomElement(['admin', 'guest']),
+                'created_at' => date("Y-m-d H:i:s"),
+            ];
+            
+            // insert semua data ke tabel secara berulang
+            $this->db->table('user')->insert($data);
+        }
+    }
+}
